01f0531b7072f6af6196bc21ae344228f396bdb9
MD Mahfuzar Rahman <mahfuzrma@hotmail.com>
2022-10-06 22:46:28 +0600

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,45 @@
+# Python code to convert Celsius scale
+# to Fahrenheit scale
+
+
+
+# Driver Code
+
+# Function call
+def j():
+    def Cel_To_Fah(n):
+        m=(n*9/5)+32
+        return m
+
+
+	# Used the formula
+	
+	
+    def Celsius_to_Kelvin(C):
+        return (C + 273.15)
+
+	
+
+
+    inp = input('enter 1 Cel_To_Fah enter 2 Celsius_to_Kelvin')
+    if (inp==1					):
+        m = input('enter Celsius')
+        n=float(m)
+
+        print(Cel_To_Fah(n))
+    else:
+        m= input('enter Celsius')
+        C=float(m)
+        print("Temperature in Kelvin ( K ) = ",
+                                            Celsius_to_Kelvin(C))
+    j()
+j()    # Python3 program to convert temperature
+# from degree Celsius to kelvin
+
+# Function to convert temperature
+# from degree Celsius to Kelvin
+# Driver Code
+
+# variable to hold the
+# temperature in Celsius
+
